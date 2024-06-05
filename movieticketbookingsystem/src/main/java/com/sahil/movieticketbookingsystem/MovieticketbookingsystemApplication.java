c63ea9e09b6aa33d1f9db40a7eff6783efc9287a
Sahil Shrivastava <skshrivastava1234@gmail.com>
2024-06-05 11:21:09 +0530

    intial commit

---
+++ movieticketbookingsystem/src/main/java/com/sahil/movieticketbookingsystem/MovieticketbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.movieticketbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieticketbookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieticketbookingsystemApplication.class, args);
+    }
+
+}
---
+++ movieticketbookingsystem/src/test/java/com/sahil/movieticketbookingsystem/MovieticketbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.movieticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieticketbookingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
